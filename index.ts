c7a23152620344628afe322e4421523b04ede1c2
Denllay <denllaay@gmail.com>
2022-10-18 19:08:03 +0300

    refactor: crypto module

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-// eslint-disable-next-line simple-import-sort/imports
-import './shim';
-
 import { AppRegistry } from 'react-native';
+
 import App from './src/app';
 
 import { name as appName } from './app.json';
+
+import './shim';
 
 AppRegistry.registerComponent(appName, () => App);
