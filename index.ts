e9380d3e8ca33f084788e286fd4b0729f527320d
Denllay <denllaay@gmail.com>
2022-10-19 19:08:07 +0300

    fix: version is not defined

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
+// eslint-disable-next-line simple-import-sort/imports
+import './shim';
+
 import { AppRegistry } from 'react-native';
 
 import App from './src/app';
 
 import { name as appName } from './app.json';
-
-import './shim';
 
 AppRegistry.registerComponent(appName, () => App);
