9867176c99d75e7130c4086adf4ba6ee4504c232
Andrei Belokopytov <belokopytov87@gmail.com>
2022-10-25 14:29:09 +0300

    feat: update button styles

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,6 +1,7 @@
 import { memo } from 'react';
 
 import { Text } from '../Text';
+import { useTheme } from '../ThemeProvider';
 import { TouchableOpacity } from '../TouchableOpacity';
 
 type Props = Omit<React.ComponentProps<typeof TouchableOpacity>, 'children'> & {
@@ -9,9 +10,17 @@ type Props = Omit<React.ComponentProps<typeof TouchableOpacity>, 'children'> & {
 };
 
 export const Button = memo(({ title, textAttirbutes, ...rest }: Props) => {
+    const { theme } = useTheme();
     return (
-        <TouchableOpacity p={23} alignItems='center' borderRadius={18} bgColor='purple' {...rest}>
-            <Text color='white' {...textAttirbutes}>
+        <TouchableOpacity
+            alignItems='center'
+            justifyContent='center'
+            borderRadius={18}
+            bgColor={theme.palette.primary100}
+            height={64}
+            {...rest}
+        >
+            <Text color={theme.palette.white} fontSize={16} fontWeight='500' {...textAttirbutes}>
                 {title}
             </Text>
         </TouchableOpacity>
--- src/shared/ui/Button/OutlineButton.tsx
+++ src/shared/ui/Button/OutlineButton.tsx
@@ -1,16 +1,19 @@
 import { memo } from 'react';
 
+import { useTheme } from '../ThemeProvider';
+
 import { Button } from './Button';
 
 type Props = React.ComponentProps<typeof Button>;
 
 export const OutlineButton = memo((props: Props) => {
+    const { theme } = useTheme();
     return (
         <Button
             bgColor='transparent'
             borderWidth={2}
-            borderColor='purple'
-            textAttirbutes={{ color: 'purple' }}
+            borderColor={theme.palette.primary100}
+            textAttirbutes={{ color: theme.palette.primary100 }}
             {...props}
         />
     );
