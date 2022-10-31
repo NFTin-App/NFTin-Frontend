2dafa3fdfa7983d80f3e4093c6d8dd9f2e54a39d
Andrei Belokopytov <belokopytov87@gmail.com>
2022-10-31 18:07:17 +0300

    feat: update Login screen

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -4,9 +4,11 @@ import { useStore } from 'effector-react';
 import { viewerModel } from '@entities/viewer';
 import { ConnectWalletButton } from '@features/auth';
 import { useNavigation } from '@react-navigation/native';
-import { OutlineButton, View } from '@shared/ui';
+import { Image, OutlineButton, SafeAreaView, View } from '@shared/ui';
 
-export const LoginPage = () => {
+import background from './images/background.png';
+
+export const Login = () => {
     const { navigate } = useNavigation();
     const isWalletConnected = useStore(viewerModel.$isConnected);
 
@@ -19,9 +21,14 @@ export const LoginPage = () => {
     }, [isWalletConnected, navigateTabs]);
 
     return (
-        <View flex={1} paddingHorizontal={16} justifyContent='center'>
-            <ConnectWalletButton />
-            <OutlineButton onPress={navigateTabs} mt={8} title='Explore the app as a guest' />
-        </View>
+        <SafeAreaView flex={1}>
+            <View flex={1} paddingHorizontal={24} paddingBottom={40} justifyContent='flex-end'>
+                <View flex={1} justifyContent='center' alignItems='center'>
+                    <Image width={245} height={273} source={background} />
+                </View>
+                <ConnectWalletButton />
+                <OutlineButton onPress={navigateTabs} mt={8} title='Explore the app as a guest' />
+            </View>
+        </SafeAreaView>
     );
 };
--- src/screens/Routing.tsx
+++ src/screens/Routing.tsx
@@ -1,5 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { LoginPage } from '@screens/Login';
+import { Login } from '@screens/Login';
 
 import { Start } from './Start';
 import { Tabs } from './Tabs';
@@ -15,7 +15,7 @@ export const Routing = () => {
             }}
         >
             <Stack.Screen name='Start' component={Start} />
-            <Stack.Screen name='Login' component={LoginPage} />
+            <Stack.Screen name='Login' component={Login} />
             <Stack.Screen name='Tabs' component={Tabs} />
         </Stack.Navigator>
     );
